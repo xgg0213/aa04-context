e0af294bb8b4ab4cd20b9d63a37570c5ddabd082
grassella <negrete.graciela@yahoo.com>
2024-04-30 22:10:16 -0500

    09-11 solved

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,4 +1,20 @@
-// Your code here 
+
+class PartyPlanner {
+
+	guestList = []
+
+	addToGuestList(name) {
+		this.guestList.push(name)
+	}
+
+	throwParty() {
+		if (this.guestList.length === 0) {
+			return "Gotta add people to the guest list"
+		}
+
+		return `Welcome to the party ${this.guestList.join(' and ')}`
+	}
+}
 
 
 
